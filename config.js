3bf7d34c87711105375f98f733d10c31e697c0e4
Kamil Bura <88596098+KamilBura@users.noreply.github.com>
2022-12-30 13:02:50 +0100

    30.12.2022 / Code Update
    + config.js
    + files / utility
    + ImportConfig Script
    
    Bot sie laduje, pobiera pliki z "config.js"

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    /**
+     * `process.env` to obiekt globalny w Node.js, ktory zawiera informacje o srodowisku uruchomieniowym procesu.
+     * Srodowisko uruchomieniowe to zestaw zmiennych srodowiskowych, ktore sa dostepne dla procesu i moga byc uzywane
+     * do konfiguracji lub dostosowania aplikacji.
+     */
+    // "token" - Bota Discord
+    token: process.env.token || "MTA1NjYxNDc0MzcwMTQ1NDkxOQ.GtI-zJ.jjYDvpsl_V8smkgHagDJwcHD3lX_exLAiR579Y",
+    // "clientId" - clientID Bota Discord
+    clientId: process.env.clientId || "1056614743701454919",
+    // "clientSecret" - Bota Discord
+    clientSecret: process.env.clientSecret || "i2rZcQcaUeAt7MZllTJXIf8IjcsLoLx0",
+}
--- structures/ShibaBot.js
+++ structures/ShibaBot.js
@@ -1,1 +1,34 @@
-const { Client } = require('discord.js');
+/**
+ * Importujemy potrzebne klasy z biblioteki "discord.js"
+ * `client` - uzywana do obslugi wydarzen, komunikacji z serwerami disocrd, itp.
+ * W pliku **index.js** obiekt klienta zostaje utworzony za pomca struktury "ShibaBot"
+ * Obiekt klienta jest uzywany w innych plikach aplikacji po jego zaimportowaniu i eksporcie za pomoca `module.exports`
+ */
+const { Client, Intents } = require('discord.js');
+const ImportConfig = require('../utility/ImportConfig');
+
+class ShibaBot extends Client {
+
+    constructor(
+        props = {
+            intents: [
+                Intents.FLAGS.GUILDS,
+                Intents.FLAGS.GUILD_VOICE_STATES,
+                Intents.FLAGS.GUILD_MESSAGES,
+            ],
+        }
+    ) {
+        super(props);
+
+        ImportConfig().then((botconfig) => {
+            this.config = botconfig;
+            this.build();
+        });
+    }
+
+    build() {
+        this.login(this.config.token);
+    } 
+}
+
+module.exports = ShibaBot;
