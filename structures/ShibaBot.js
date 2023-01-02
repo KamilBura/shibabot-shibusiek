fa52056c3a991524a99499128363da67d3312d09
Kamil Bura <88596098+KamilBura@users.noreply.github.com>
2023-01-02 22:52:16 +0100

    02.01.2022 Comments Update
    Dodanie zmiennej ktora sprawdza czy w pliku /commands/slashCommands znajduje sie jakis plik. jak nie daje nam o tym znac w konsoli.
    
     Dodanie Komentarzy do kodu LoadCommands()

--- structures/ShibaBot.js
+++ structures/ShibaBot.js
@@ -1,14 +1,20 @@
 /**
+ * 
  * Importujemy potrzebne klasy z biblioteki "discord.js"
  * `client` - uzywana do obslugi wydarzen, komunikacji z serwerami disocrd, itp.
  * W pliku **index.js** obiekt klienta zostaje utworzony za pomca struktury "ShibaBot"
  * Obiekt klienta jest uzywany w innych plikach aplikacji po jego zaimportowaniu i eksporcie za pomoca `module.exports`
+ * 
  */
+
+// Bilbioteki / Module
 const { Client, IntentsBitField, Collection } = require('discord.js');
-const ImportConfig = require('../utility/ImportConfig');
-const CommandLog = require('../library/CommandLog');
 const path = require('path');
 const fs = require('fs');
+
+// Biblioteki
+const ImportConfig = require('../utility/ImportConfig');
+const CommandLog = require('../library/CommandLog');
 
 // Nowa klasa o nazwie "ShibaBot", ktory rozszerza klase 'Client' z biblioteki discord.js
 class ShibaBot extends Client {
@@ -37,7 +43,16 @@ class ShibaBot extends Client {
 
         this.CommandLog = new CommandLog(path.join(__dirname, "..", "output.log"));
 
+        // Wywolujemy Funkcje "LoadCommands" L=65
         this.LoadCommands();
+
+        /**
+         * 
+         * Tworzymy nowa kolekcje o nazwie "slashCommands"
+         * Collection jest rodzajem danych sluzacym do przechowywania i zarzadzania tablicami. Collection z discord.js rozni sie tym, ze
+         * Collection mozliwia dodawanie wartosci do tablicy poprzez uzycie kluczy, a takze udostepnia dodatkowe metody jak .find() czy .filter()
+         * 
+         */
         this.slashCommands = new Collection();
 
     }
@@ -60,22 +75,37 @@ class ShibaBot extends Client {
         this.login(this.config.token);
     }
 
+    // "LoadCommands" - wykonywany kod pod tym znaczeniem ktory jest trigerowany this.xxx();
     LoadCommands() {
+        // "path.join" - Laczenie sciezki w jedna, kierowana sciezka to ../commands/slashCommands/
         let SlashCommandsDir = path.join(
             __dirname, "..", "commands", "slashCommands"
         );
         
+        // Odczytujemy zawartosc folderu ktory znajduje sie u gory okreslonej zmiennej "SlashCommandsDir"
+        // Przypisujemy 2 zmienne (error i files)
         fs.readdir(SlashCommandsDir, (error, files) => {
+            // Jezeli wystapi blad to zostanie wybrana ta sciezka
             if (error) {
+                // Zostanie pokazany blad
                 throw error;
+            } else if (files.length === 0) {
+                this.warn("slashCommands folder seems to be empty!")
             } else {
+                // Jezeli nie bedzie problemu wybierana jest 2 sciezka
+                // .forEach - przechodzi przez kazdy Plik ktory sie znajduje w SlashCommandsDir
                 files.forEach((file) => {
+                    // Kazdy plik jest wczytywany za pomoca funkcji "require"
                     let command = require(SlashCommandsDir + "/" + file);
+                    // Plik jest sprawdzany czy znajduje sie w pliku odpowiedna zmienna "run"
                     if (!command || !command.run) {
+                        // Jezeli odpowiednia zmienna nie jest ustalony wczytuje blad
                         return this.warn("It was unable to load Command starting with: " + file.split(".")[0]);
                     }
 
+                    // Jezeli jest okreslona funkcja to wczytuje kolekcje za pomoca "this.slashCommands" uzywajac metody "set"
                     this.slashCommands.set(file.split(".")[0].toLowerCase(), command);
+                    // Wyswietla informacje w consoli ze komenda zostala pomyslnie zainicjowana
                     this.log("Slash Command " + "'" + file.split(".")[0] + "'" + "was loaded successfully.");
                 });
             }
