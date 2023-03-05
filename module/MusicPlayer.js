ace939f9c9295c8829e6edc9486d9b8a7ddec885
Kamil Bura <88596098+KamilBura@users.noreply.github.com>
2023-03-05 19:51:31 +0100

    05.03.2023 / MusicPlayer
    Checked if message is deleted, if true it marks with help from discord.js

--- module/MusicPlayer.js
+++ module/MusicPlayer.js
@@ -2,33 +2,54 @@ const { Message } = require("discord.js");
 const { Structure } = require("erela.js");
 const Client = require("../structures/ShibaBot");
 
-// Rozrzerzamy Structure z Biblioteki "erela.js", rozrzerzamy "Player"
+//? Rozrzerzamy Structure z Biblioteki "erela.js", rozrzerzamy "Player"
 Structure.extend(
     "Player",
     (Player) => class extends Player {
         constructor(...props) {
             super(...props)
-            // Ustawiamy "24/7" na false
+            //! Ustawiamy "24/7" na false
             this.twentyFourSeven = false;
         }
         
-        // Sprawdzanie czy "pausedMessage" jest zdefinowana i czy nie zostala usunieta. 
-        ResumeMessage(client, message) {
+        //? Sprawdzanie czy "pausedMessage" jest zdefinowana i czy nie zostala usunieta. 
+        FuncResumeMessage(client, message) {
+            //? Jezeli "pausedMessage" 'i' nie jest oznaczona jako usunieta 
+            if (this.pausedMessage && !client.isMessageDeleted(this.pausedMessage)) {
+                //? Usuwamy wiadomosc "pausedMessage"
+                this.pausedMessage.delete();
+                //? Zaznaczami Wiadomosc jako usunieta za pomoca "markMessageAsDeleted" z discord.js
+                client.markMessageAsDeleted(this.pausedMessage);
+                
+            }
+            //? Zwracamy wiadomosc i przypisujemy ja do "resumeMessage"
+            return (this.resumeMessage = message);
 
         }
 
-        // Sprawdzanie czy "resumeMessage" jest zdefinowana i czy nie zostala usunieta
-        PausedMessage(client, message) {
+        //? Sprawdzanie czy "resumeMessage" jest zdefinowana i czy nie zostala usunieta
+        FuncPausedMessage(client, message) {
+            if (this.resumeMessage && !client.isMessageDeleted(this.resumeMessage)) {
+                this.resumeMessage.delete();
+                client.markMessageAsDeleted(this.resumeMessage);
+                
+            }
+            return (this.pausedMessage = message);
 
         }
 
-        // Sprawdzanie czy "NowPlayingMessage" jest zdefiniowana i czy nie zostala usunieta.
-        NowPlayingMessage(client, message) {
+        //? Sprawdzanie czy "NowPlayingMessage" jest zdefiniowana i czy nie zostala usunieta.
+        FuncNowPlayingMessage(client, message) {
+            if (this.nowPlayingMessage && !client.isMessageDeleted(this.nowPlayingMessage)) {
+                this.nowPlayingMessage.delete();
+                client.markMessageAsDeleted(this.nowPlayingMessage);
+
+            }
+            return (this.nowPlayingMessage = message);
 
         }
 
-        // !WILL BE DONE LATER!
 
 
-    }
-)
+    },
+);
